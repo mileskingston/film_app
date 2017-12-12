ae627d40a90876aa06c574f77790fc395afcf46a
Miles Kingston <miles.kingston@dixonscarphone.com>
2017-12-12 14:26:25 +0000

    rating component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Rating from './Rating';
+import logo from './images/logo.svg';
+import searchIcon from './images/search.svg';
+import filmPoster from './images/film-poster.jpg';
 import './App.css';
+
+const product = {
+  title: 'House of Cards',
+  poster_path: filmPoster,
+  rating: {
+    score: 9.3,
+    total: 300
+  }
+};
 
 class App extends Component {
   render() {
@@ -8,17 +20,31 @@ class App extends Component {
       <div className="app">
         <header className="app__header">
           <img src={logo} className="app__logo" alt="logo" />
-          <h1 className="app__title">Welcome to React</h1>
+
+          <form className="app__form">
+            <input type="text" className="app__input"/>
+            <button type="button" className="app__btn">
+              <img src={searchIcon} alt="search icon" />
+            </button>
+          </form>
+          {/* <h1 className="app__title">Film search</h1> */}
         </header>
 
-        <form>
-          <input type="text" className="app__form-control"/>
-          <button type="button" className="app__btn"></button>
-        </form>
+        <div className="wrapper">
+          <section>
+            <h2>Lastest films</h2>
 
-        <section>
-          
-        </section>
+            <div className="app__films">
+              <div className="app__film">
+                <img src={product.poster_path} alt={product.title} />
+                <h3>{product.title}</h3>
+                {Rating &&
+                  <Rating rating={product.rating} />
+                }
+              </div>            
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
---
+++ src/Rating.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Rating.css';
+
+class Rating extends Component {
+  render() {
+    const {props} = this;
+    const classes = ['rating__score'];
+
+    if (props.rating.score < 5) {
+      classes.push('rating__score--low');
+    } else if (props.rating.score > 6) {
+      classes.push('rating__score--high');
+    } else {
+      classes.push('rating__score--mid');      
+    }
+
+    return (
+      <div className="app__rating">
+        <span className={classes.join(' ')}>{props.rating.score}</span>
+
+        {props.showTotal &&
+          <span className="rating__total">{props.rating.total}</span>
+        }
+      </div>
+    );
+  }
+}
+
+Rating.displayName = 'Rating';
+
+Rating.propTypes = {
+  rating: PropTypes.shape({
+    score: PropTypes.number,
+    total: PropTypes.number
+  }),
+  showTotal: PropTypes.bool
+};
+
+Rating.defaultProps = {
+  showTotal: false
+};
+
+export default Rating;
